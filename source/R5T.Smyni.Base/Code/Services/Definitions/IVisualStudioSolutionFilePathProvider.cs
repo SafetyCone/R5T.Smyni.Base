2262a4654c888546d159a1a88510764f3f344c4f
MinexAutomation <minex.automation@gmail.com>
2019-12-16 14:44:31 -0800

    Added a Visual Studio solution file path provider service.

---
+++ source/R5T.Smyni.Base/Code/Services/Definitions/IVisualStudioSolutionFilePathProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Smyni
+{
+    public interface IVisualStudioSolutionFilePathProvider
+    {
+        string GetVisualStudioSolutionFilePath(string solutionDirectoryPath, string solutionName);
+    }
+}
