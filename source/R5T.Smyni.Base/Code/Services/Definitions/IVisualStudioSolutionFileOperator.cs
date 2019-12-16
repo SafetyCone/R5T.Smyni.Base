83a5aa8a7b6027aecdc027d6394336cbf603651f
MinexAutomation <minex.automation@gmail.com>
2019-12-16 10:34:05 -0800

    Added Visual Studio solution-file operator.

---
+++ source/R5T.Smyni.Base/Code/Services/Definitions/IVisualStudioSolutionFileOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Smyni
+{
+    /// <summary>
+    /// Performs operations on a Visual Studio solution file.
+    /// </summary>
+    public interface IVisualStudioSolutionFileOperator
+    {
+    }
+}
