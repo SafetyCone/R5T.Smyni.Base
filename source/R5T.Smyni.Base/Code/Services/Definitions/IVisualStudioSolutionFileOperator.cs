a38832e7377cfaee3d5bf28e7b2fbd8e92c8e253
davidcoats <david@getsafetycone.com>
2022-04-27 15:35:00 -0700

    Interim changes.

--- source/R5T.Smyni.Base/Code/Services/Definitions/IVisualStudioSolutionFileOperator.cs
+++ source/R5T.Smyni.Base/Code/Services/Definitions/IVisualStudioSolutionFileOperator.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Smyni
-{
+{[ServiceDefinitionMarker]
     /// <summary>
     /// Performs operations on a Visual Studio solution file.
     /// </summary>
-    public interface IVisualStudioSolutionFileOperator
+    public interface IVisualStudioSolutionFileOperator:IServiceDefinition
     {
         string CreateSolutionFile(string solutionDirectoryPath, string solutionName);
 
